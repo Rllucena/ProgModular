1523462133cca5b60b461cbe406d1efbebddb2b7
Rllucena <rafael_l_lucena@hotmail.com>
2023-06-05 20:12:47 -0300

    teste

---
+++ aula10/calcMaior.java
@@ -0,0 +1,46 @@
+package aula10;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class calcMaior {
+    
+    public static int calculaMaior(int[] n){
+
+        int i,r;
+        r=n[0];
+        for(i=0;i<n.length;i++){
+           if (n[i]>r) r=n[i]; 
+        };
+        return r;
+    }
+
+
+
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+
+        int i,n;
+        
+        System.out.println("Digite o numero de elementos do vetor: ");
+        n = in.nextInt();
+        
+        int num[]=new int [n];
+
+        for(i=0;i<n;i++){
+            
+            try{
+            System.out.println("Digite o valor do indice " + i+":");
+            num[i] = in.nextInt();
+            }catch (InputMismatchException e) {
+                System.out.println("valor inválido, digite novamente: ");
+                i--;
+                in.nextLine();
+            }
+         };
+
+        System.out.println("maior dos elementos: " + calculaMaior(num));
+
+        in.close();
+    }
+}
